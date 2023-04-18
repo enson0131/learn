e7b336a6cff64bf2b328553e22c3c86a50499b59
enson_chen <1004714132@qq.com>
2023-04-18 09:48:06 +0800

    feat: ✨ 合并链表

--- LeetCode/08 链表相关专题/21 合并链表/index.js
+++ LeetCode/08 链表相关专题/21 合并链表/index.js
@@ -1,3 +1,8 @@
+/**
+ 真题描述：将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有结点组成的。
+ 示例： 输入：1->2->4, 1->3->4 输出：1->1->2->3->4->4
+ */
+
 function mergeTwoLists(list1, list2) {
     let head = new NodeList();
     let cur = head;
@@ -25,4 +30,49 @@ function mergeTwoLists(list1, list2) {
     head = head.next;
 
     return head;
+}
+
+
+
+function mergeTwoLists(list1, list2) {
+  const head = new ListNode();
+  let pre = head;
+  let cur1 = list1;
+  let cur2 = list2;
+
+  while(cur1 && cur2) {
+    const val1 = cur1.val;
+    const val2 = cur2.val;
+
+    if (val1 <= val2) {
+      pre.next = cur1;
+      cur1 = cur1.next;
+    } else {
+      pre.next = cur2;
+      cur2 = cur2.next;
+    }
+
+    pre = pre.next;
+  }
+
+  // while(cur1) {
+  //   pre.next = cur1;
+  //   cur1 = cur1.next;
+  //   pre = pre.next;
+  // }
+
+  // while(cur2) {
+  //   pre.next = cur1;
+  //   cur1 = cur1.next;
+  //   pre = pre.next;
+  // }
+  if (cur1) {
+    pre.next = cur1;
+  }
+
+  if (cur2) {
+    pre.next = cur2;
+  }
+
+  return head.next;
 }
