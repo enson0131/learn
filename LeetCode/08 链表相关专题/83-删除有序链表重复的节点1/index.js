c3b2c50d579c256b62809de8a821dda2120d78c9
enson_chen <1004714132@qq.com>
2023-05-21 14:59:16 +0800

    feat: ✨ 删除有序链表中的相同节点

--- LeetCode/08 链表相关专题/83-删除有序链表重复的节点1/index.js
+++ LeetCode/08 链表相关专题/83-删除有序链表重复的节点1/index.js
@@ -1,3 +1,7 @@
+/**
+ 由于此处是已排序的链表
+ 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ */
 function deleteDuplicates(head) {
     let cur = head;
     let pre = null;
@@ -10,6 +14,22 @@ function deleteDuplicates(head) {
         pre = cur;
         cur = cur.next;
       }
+    }
+
+    return head;
+}
+
+
+
+function deleteDuplicates(head) {
+    let cur  = head;
+
+    while(cur && cur.next) {
+      if (cur.val === cur.next.val) {
+        cur.next = cur.next.next;
+      }
+
+      cur  = cur.next;
     }
 
     return head;
