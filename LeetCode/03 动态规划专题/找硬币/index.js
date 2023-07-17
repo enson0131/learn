834b0246122911c228a2e96b80037363f353527c
enson_chen <1004714132@qq.com>
2023-07-17 10:33:06 +0800

    feat: ✨ 找硬币

--- LeetCode/03 动态规划专题/找硬币/index.js
+++ LeetCode/03 动态规划专题/找硬币/index.js
@@ -61,3 +61,38 @@ const getMinValue2 = (coins, amount) => {
 }
 
 console.log(`getMin ===>`, getMinValue2([1, 2, 5], 11))
+
+
+/**
+ 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。 
+ 示例1：
+    输入: coins = [1, 2, 5], amount = 11
+    输出: 3
+    解释: 11 = 5 + 5 + 1
+ 思路 （总金额一定是通过 coins 内部的硬币组成的）
+ 对总金额不断的减去硬币的面值，直到减不动为止
+    拿走 c1
+    拿走 c2
+    拿走 c3
+    ......
+    拿走 cn
+ f(n) = Math.min(f(n - C1), f(n - C2), f(n - C3), ....f(n - Cn)) + 1;
+ */
+const getMinValue3 = (coins, amount) => {
+    const f = [];
+    f[0] = 0;
+    
+    for(let i = 1; i <= amount; i++) {
+        f[i] = Infinity;
+        for(let c of coins) {
+            if (i - c > 0) {
+                f[i] = Math.min(f[i - c] + 1, f[i]);
+            }
+        }
+    }
+
+    if (f[i] === Infinity) return -1;
+
+    return f[i];
+    
+}
