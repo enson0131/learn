64211a7698208884cdf5c3d551e45e22c59a1bb8
enson_chen <1004714132@qq.com>
2023-11-09 10:25:56 +0800

    feat: ✨ 添加配置

--- 手写/偏函数.js
+++ 手写/偏函数.js
@@ -1,8 +1,7 @@
-
 // partial application
 // 偏函数的好处在于可以固定参数，减少重复参数的传递
 const warp = (fn, ...args) => {
-    return (...params) => {
-        fn(...args, ...params);
-    }
-}
+  return (...params) => {
+    fn(...args, ...params);
+  };
+};
