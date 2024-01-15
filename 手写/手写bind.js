a963b4d4b716f444ebbb43da36bb1dce45b0b34a
enson_chen <1004714132@qq.com>
2024-01-15 15:56:35 +0800

    feat: ✨ 查看 new、call、apply、bind 的实现逻辑

--- 手写/手写bind.js
+++ 手写/手写bind.js
@@ -1,187 +1,179 @@
 function myBind(ctx, ...args) {
-    const fn = this;
+  const fn = this;
 
-    return function (...rest) {
-        fn.apply(ctx, [...args, ...rest])
-    }
+  return function (...rest) {
+    fn.apply(ctx, [...args, ...rest]);
+  };
 }
-
 
 function myBind_pro(ctx, ...args) {
-    const fn = this;
-    
-    const noop = function () {}
+  const fn = this;
 
-    const res = function(...rest) {
-        fn.apply(this instanceof noop ? this : ctx, [...args, ...rest])
-    }
+  const noop = function () {};
 
-    if (fn.prototype) {
-        noop.prototype = fn.prototype;
-    }
+  const res = function (...rest) {
+    fn.apply(this instanceof noop ? this : ctx, [...args, ...rest]);
+  };
 
-    res.prototype = new noop();
+  if (fn.prototype) {
+    noop.prototype = fn.prototype;
+  }
 
-    return res;
+  res.prototype = new noop();
+
+  return res;
 }
-
 
 // ---- 再次手写一波 ------
-Function.prototype.myBind = function(ctx, ...args) {
-    const fn = this;
-    const noop = function() {};
-    const res = function(...rest) {
-        return fn.apply(ctx, [...args, ...rest]);
-    }
+Function.prototype.myBind = function (ctx, ...args) {
+  const fn = this;
+  const noop = function () {};
+  const res = function (...rest) {
+    return fn.apply(ctx, [...args, ...rest]);
+  };
 
-    if (fn.prototype) {
-        noop.prototype = fn.prototype;
-    }
-    
-    res.prototype = new noop;
+  if (fn.prototype) {
+    noop.prototype = fn.prototype;
+  }
 
-    return res;
-}
+  res.prototype = new noop();
+
+  return res;
+};
 
 /**
  * fn.call(window, 1, 2, 3, 4)
- * @param {*} ctx 
- * @param  {...any} args 
+ * @param {*} ctx
+ * @param  {...any} args
  */
 Function.prototype.myCall = function (ctx, ...args) {
-    const fn = this;
-    const onlyFlag = Symbol();
-    const obj = ctx ? ctx : window;
-    obj[onlyFlag] = fn;
+  const fn = this;
+  const onlyFlag = Symbol();
+  const obj = ctx ? ctx : window;
+  obj[onlyFlag] = fn;
 
-    const result = obj[onlyFlag](...args);
-    delete obj[onlyFlag];
+  const result = obj[onlyFlag](...args);
+  delete obj[onlyFlag];
 
-    return result;
-}
+  return result;
+};
 
 /**
  * fn.apply(window, [1, 2, 3, 4])
- * @param {*} ctx 
- * @param  {...any} args 
+ * @param {*} ctx
+ * @param  {...any} args
  */
- Function.prototype.myApply = function (ctx, args) {
-    const fn = this;
-    const onlyFlag = Symbol();
-    const obj = ctx || window;
-    obj[onlyFlag] = fn;
+Function.prototype.myApply = function (ctx, args) {
+  const fn = this;
+  const onlyFlag = Symbol();
+  const obj = ctx || window;
+  obj[onlyFlag] = fn;
 
-    const result = obj[onlyFlag](...args);
-    delete obj[onlyFlag];
+  const result = obj[onlyFlag](...args);
+  delete obj[onlyFlag];
 
-    return result;
-}
-
+  return result;
+};
 
 function _new(fn, ...args) {
-    const obj = Object.create(fn.prototype);
-   
-    const result = fn.call(obj, ...args);
+  const obj = Object.create(fn.prototype);
 
-    if (typeof result === 'object' && result) {
-        return result;
-    } else {
-        return obj;
-    }
+  const result = fn.call(obj, ...args);
+
+  if (typeof result === "object" && result) {
+    return result;
+  } else {
+    return obj;
+  }
 }
 
 // instanceof: 判断 fn.prototype 是否在obj的原型上
-function _instanceof (obj, fn) {
-    if (!obj) return;
-    let parentObj = Object.getPrototypeOf(obj); // 获取原型对象
+function _instanceof(obj, fn) {
+  if (!obj) return;
+  let parentObj = Object.getPrototypeOf(obj); // 获取原型对象
 
-    while(parentObj) {
-        if (parentObj === fn.prototype) return true;
-        parentObj = Object.getPrototypeOf(parentObj); // 获取原型对象
-    }
+  while (parentObj) {
+    if (parentObj === fn.prototype) return true;
+    parentObj = Object.getPrototypeOf(parentObj); // 获取原型对象
+  }
 
-    return false;
+  return false;
 }
 
-console.log(_instanceof({}, Object))
-console.log(_instanceof(Object.prototype, Object))
+console.log(_instanceof({}, Object));
+console.log(_instanceof(Object.prototype, Object));
 
-const checkIsObj = value => {
-    return typeof value === 'object' && value;
+const checkIsObj = (value) => {
+  return typeof value === "object" && value;
 };
 
 const hash = new WeakMap();
 
-const _deepClone = function(obj) {
-    if (!checkIsObj(obj)) return obj;
+const _deepClone = function (obj) {
+  if (!checkIsObj(obj)) return obj;
 
-    if (hash.has(obj)) return hash.get(obj);
+  if (hash.has(obj)) return hash.get(obj);
 
-    let result = {};
-    if (Array.isArray(obj)) {
-        result = [];
-    }
+  let result = {};
+  if (Array.isArray(obj)) {
+    result = [];
+  }
 
-    hash.set(obj, result);
+  hash.set(obj, result);
 
-    for (const key in obj) {
-        result[key] = _deepClone(obj[key]);
-    }
+  for (const key in obj) {
+    result[key] = _deepClone(obj[key]);
+  }
 
-    return result;
+  return result;
 };
 
-
-
-
 function debounce(fn, delay = 100) {
-    let timer;
-    return function(...rest) {
-        if (timer) clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn.call(this, ...rest);
-            clearTimeout(timer)
-        }, delay)
-    }
+  let timer;
+  return function (...rest) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.call(this, ...rest);
+      clearTimeout(timer);
+    }, delay);
+  };
 }
 
-
-function throttle (fn, delay = 100) {
-    let timer;
-    return function(...rest) {
-        if (timer) return;
-        timer = setTimeout(() => {
-            fn.call(this, ...rest);
-            clearTimeout(timer);
-        }, delay)
-    }
+function throttle(fn, delay = 100) {
+  let timer;
+  return function (...rest) {
+    if (timer) return;
+    timer = setTimeout(() => {
+      fn.call(this, ...rest);
+      clearTimeout(timer);
+    }, delay);
+  };
 }
 
 function _flat(arr) {
-    const hasArr = arr.some(item => item instanceof Array);
+  const hasArr = arr.some((item) => item instanceof Array);
 
-    if (!hasArr) return arr;
+  if (!hasArr) return arr;
 
-    // 拍平
-    const result = Array.prototype.concat.apply([], arr);
-    return _flat(result);
+  // 拍平
+  const result = Array.prototype.concat.apply([], arr);
+  return _flat(result);
 }
 console.log(_flat([1, 2, [1, [2, 3, [4, 5, [6]]]]]));
 
-// fn(1)(2, 3) 
+// fn(1)(2, 3)
 // _myCurry(fn, 1)(2, 3)
 function _myCurry(fn, ...args) {
-    const len = fn.length;
-    let allArgs = [...args];
-    const res = function(...params) {
-        allArgs = [...allArgs, ...params];
-        if (allArgs.length === len) {
-            return fn(...allArgs)
-        } else {
-            return res;
-        }
+  const len = fn.length;
+  let allArgs = [...args];
+  const res = function (...params) {
+    allArgs = [...allArgs, ...params];
+    if (allArgs.length === len) {
+      return fn(...allArgs);
+    } else {
+      return res;
     }
+  };
 
-    return res;
+  return res;
 }
-
