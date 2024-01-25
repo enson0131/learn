4f31870c4fe16526a89b810c5d97e3ab3c0ba1ef
enson_chen <1004714132@qq.com>
2024-01-25 18:01:06 +0800

    feat: ✨ 添加react-router的实现

---
+++ React相关/router/my-react-app/src/components/Home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>主页</div>;
+};
+
+export default Home;
--- React相关/router/my-react-app/src/main.tsx
+++ React相关/router/my-react-app/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import User from "./components/User";
+import Profile from "./components/Profile";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Router>
+    <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="/user" Component={User} />
+      <Route path="/profile" Component={Profile} />
+    </Routes>
+  </Router>
+);
