c2053a9a2c6154cefd4020d12bc3c029aded049d
enson_chen <1004714132@qq.com>
2024-10-29 09:20:44 +0800

    feat: ✨ 失焦事件修改 state 会导致点击事件失效

--- React相关/router/my-react-app/src/components/Home/index.tsx
+++ React相关/router/my-react-app/src/components/Home/index.tsx
@@ -1,5 +1,41 @@
+import { memo, useState } from "react";
+
 const Home = () => {
-  return <div>主页</div>;
+  const [value, setValue] = useState(0);
+
+  const Comp1 = () => (
+    console.log("创建子组件"),
+    (
+      <button
+        onClick={(e) => {
+          console.log("触发点击事件handleClick");
+          e.preventDefault();
+        }}
+        onPointerDown={() => console.log(`onPointerDown`)}
+        onPointerUp={() => console.log(`onPointerUp`)}
+      >
+        点击事件
+      </button>
+    )
+  );
+
+  console.log("2");
+
+  return (
+    <>
+      <div>主页</div>
+      <div>失焦次数：{value}</div>
+      <input
+        onBlur={() => {
+          // setTimeout(() => {
+          console.log("1");
+          setValue(value + 1);
+          // }, 1000);
+        }}
+      />
+      <Comp1 />
+    </>
+  );
 };
 
 export default Home;
