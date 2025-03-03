fdf3c9ef07fdd8c84308f1665d222cdb20689467
enson_chen <1004714132@qq.com>
2025-03-03 10:33:08 +0800

    fix: 🐞 回顾 promise 的 then、catch 不是函数的长久

--- 代码输出/题目15.js
+++ 代码输出/题目15.js
@@ -1,23 +1,23 @@
 const async1 = async () => {
-  console.log('async1');
+  console.log("async1");
   setTimeout(() => {
-    console.log('timer1')
-  }, 2000)
-  await new Promise(resolve => {
-    console.log('promise1')
-  })
-  console.log('async1 end')
-  return 'async1 success'
-} 
-console.log('script start');
-async1().then(res => console.log(res));
-console.log('script end');
-// .then和.catch必须是一个函数，如果不是函数，会创建一个函数并将上一次返回的值作为函数返回值
+    console.log("timer1");
+  }, 2000);
+  await new Promise((resolve) => {
+    console.log("promise1");
+  });
+  console.log("async1 end");
+  return "async1 success";
+};
+console.log("script start");
+async1().then((res) => console.log(res));
+console.log("script end");
+// ⚠️ Note: .then和.catch必须是一个函数，如果不是函数，会创建一个函数并将上一次返回的值作为函数返回值！
 Promise.resolve(1)
   .then(2)
   .then(Promise.resolve(3))
   .catch(4)
-  .then(res => console.log(res))
+  .then((res) => console.log(res)); // 输出1
 setTimeout(() => {
-  console.log('timer2')
-}, 1000) 
+  console.log("timer2");
+}, 1000);
