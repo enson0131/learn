3232ad707402d189f6e6f5ecbe02a969223bc95e
enson_chen <1004714132@qq.com>
2025-06-10 09:32:15 +0800

    fix: 修复 XMLHttpRequest 代码格式和错误处理逻辑
    调整了代码的格式，确保函数结束符号正确，并修复了错误处理的输出格式，使其更加一致和清晰。

--- xhr/xhr.js
+++ xhr/xhr.js
@@ -8,20 +8,20 @@ xhr.onreadystatechange = function () {
   } else {
     console.log(`请求成功 - ${this.status}: `, this);
   }
-}
+};
 
 xhr.onerror = function () {
-  console.log(`请求失败 - ${this.status}: `, this.statusText)
-}
+  console.log(`请求失败 - ${this.status}: `, this.statusText);
+};
 
 xhr.setRequestHeader("Accept", "*/*");
 xhr.responseType = "json";
 setTimeout(() => {
-    xhr.send("t=dc&aip=1&_r=3&v=1&_v=j96&tid=UA-36116321-5&cid=830505092.1621180818&jid=1026285878&gjid=1584384230&_gid=2074967334.1638599963&_u=QACAAAAAAAAAAC~&z=847299491");
-    xhr.abort();
-}, 2000)
-
-
+  xhr.send(
+    "t=dc&aip=1&_r=3&v=1&_v=j96&tid=UA-36116321-5&cid=830505092.1621180818&jid=1026285878&gjid=1584384230&_gid=2074967334.1638599963&_u=QACAAAAAAAAAAC~&z=847299491"
+  );
+  xhr.abort();
+}, 2000);
 
 // var obj = {
 //   a:1,
@@ -51,11 +51,10 @@ setTimeout(() => {
 
 // xhr.open(`GET`, 'XXXX', true);
 
-
 // xhr.onreadystatechange = function () {
 //    if (this.readyState === 4 && this.status === 200) {
 
-//    } 
+//    }
 // }
 
 // xhr.send();
